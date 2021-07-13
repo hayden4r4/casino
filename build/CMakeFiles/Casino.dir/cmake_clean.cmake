0027c7e08d97bab9a64cf40631c150ae9a14f865
hayden4r4 <hayden4r4@gmail.com>
2021-07-13 17:39:50 -0500

    Added CMake

---
+++ build/CMakeFiles/Casino.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Casino.dir/main.cpp.o"
+  "CMakeFiles/Casino.dir/main.cpp.o.d"
+  "Casino"
+  "Casino.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Casino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
